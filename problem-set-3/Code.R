7ca05659201255943a6a2b08bc20d4123ec414b3
jrrHernandez <123601048+jrrHernandez@users.noreply.github.com>
2023-05-20 15:44:52 -0500

    Update Code.R

--- problem-set-3/Code.R
+++ problem-set-3/Code.R
@@ -25,18 +25,27 @@ stargazer(modelo_1,modelo_2,modelo_3,
 ##DATOS ESPACIALES##
 
 #descargar datos
-restaurantes <- opq(bbox = getbb("Cucuta Colombia")) %>%
+restaurantes <- opq(bbox = getbb("Bogota")) %>%
                 add_osm_feature(key = "amenity", value = "restaurant") %>%
                 osmdata_sf() %>% .$osm_points %>% select(osm_id, name)
 
 
-parques <- opq(bbox = getbb("Cucuta Colombia")) %>%
+parques <- opq(bbox = getbb("Bogota")) %>%
            add_osm_feature(key = "leisure", value = "park") %>%
            osmdata_sf() %>% .$osm_polygons %>% select(osm_id,name)
 
-#
-leaflet() %>% addTiles() %>% addPolygons(data=parques)
 
+#Visualizaciones
+leaflet() %>% addTiles() %>% addPolygons(data= parques)
+leaflet() %>% addTiles() %>% addCircles(data=restaurantes)
+
+#geocodificar direcciones
+geocode_OSM("calle 27a #22-05", as.sf=T)
+
+qqta <- st_intersection(x= parques, y= restaurantes)
+
+
+#exportar 
 
 
 
